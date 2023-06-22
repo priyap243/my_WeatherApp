7e192860ea4c9bdfe1e86c83273e938ee3968c23
priyap243 <priyap2403@gmail.com>
2023-06-22 13:52:54 +0100

    JS file
    added JS file

---
+++ src/weatherApp.js
@@ -0,0 +1,102 @@
+let now = new Date();
+
+let h1 = document.querySelector("h1");
+let h2 = document.querySelector("h2");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let day = days[now.getDay()];
+let month = months[now.getMonth()];
+let date = now.getDate();
+
+h1.innerHTML = `${hours}:${minutes}`;
+
+h2.innerHTML = `${day}, ${date} ${month}`;
+
+function displayWeatherCondition(response) {
+  console.log(response.data);
+  document.querySelector(".time-zone").innerHTML = response.data.name;
+  document.querySelector(".current-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#pressure").innerHTML = Math.round(
+    response.data.main.pressure
+  );
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#visibility").innerHTML = Math.round(
+    response.data.visibility / 1000
+  );
+  document.querySelector("#precipitation").innerHTML = Math.round(
+    response.data.main.pressure
+  );
+  document.querySelector("#country").innerHTML = response.data.sys.country;
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].description;
+}
+
+function searchCity(city) {
+  let apiKey = "b87b61c011c1d75a7df966f24970e58f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "b87b61c011c1d75a7df966f24970e58f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let form = document.querySelector(".form-group");
+form.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Cairo");
